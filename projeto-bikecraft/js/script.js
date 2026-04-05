78860abd2df66f67c5854c15db0bd7e4a962ecfa
GitBrowserBit <browsergit@gmail.com>
2026-04-05 11:55:25 -0300

    definido fuçao de esconder resposta e exibir em perguntas frequentes do site

--- projeto-bikecraft/js/script.js
+++ projeto-bikecraft/js/script.js
@@ -24,3 +24,23 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto)
+
+//perguntas frequente
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa')
+    const ativa = resposta.classList.contains("ativa")
+    pergunta.setAttribute('aria-expanded', ativa);
+
+}
+
+function eventPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventPerguntas);
